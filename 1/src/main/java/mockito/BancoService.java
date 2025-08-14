03ec1b816d63a202eb8d58765c111eddb8873d98
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-14 09:36:28 -0300

    feat: implementando operacoes bancarias no service de Banco

--- 1/src/main/java/mockito/BancoService.java
+++ 1/src/main/java/mockito/BancoService.java
@@ -1,4 +1,20 @@
 package mockito;
 
 public class BancoService {
+    private ContaRepository contaRepository;
+
+
+    public BancoService(ContaRepository contaRepository) {
+        this.contaRepository = contaRepository;
+    }
+
+
+    public double consultarSaldo(String numeroConta) {
+        return contaRepository.buscarConta(numeroConta).getSaldo();
+    }
+
+
+    public void depositar(String numeroConta, double valor) {
+        contaRepository.buscarConta(numeroConta).setSaldo(valor);
+    }
 }
