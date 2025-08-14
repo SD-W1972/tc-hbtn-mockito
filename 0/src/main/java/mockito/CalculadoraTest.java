6f5fec89cecae97bbc4f74be65944f2856edd327
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-14 08:02:23 -0300

    feat: adicionando mock da interface servico matematico e preparando para instanciar a calculadora

--- 0/src/main/java/mockito/CalculadoraTest.java
+++ 0/src/main/java/mockito/CalculadoraTest.java
@@ -1,4 +1,18 @@
 package mockito;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class CalculadoraTest {
+
+    @Mock
+    private ServicoMatematico servicoMatematico;
+
+    private Calculadora calculadora;
+
 }
