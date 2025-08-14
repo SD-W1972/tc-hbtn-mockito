e84cddc192c77d0c52be7a60784e3844c71425f5
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-14 09:10:03 -0300

    feat mudando o arquivo de testes para seu devido diretório

--- 0/src/test/java/CalculadoraTest.java
+++ 0/src/test/java/CalculadoraTest.java
@@ -1,5 +1,5 @@
-package mockito;
-
+import mockito.Calculadora;
+import mockito.ServicoMatematico;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
